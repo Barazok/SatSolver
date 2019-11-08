b4ce9dcc0fa9b7b673f36c0ed23c17204ea02b68
flopal <floret.palanque@hotmail.com>
2019-11-08 16:52:47 +0100

    Beautify fml.hs

--- src/Data/Algorithm/Sat/Fml.hs
+++ src/Data/Algorithm/Sat/Fml.hs
@@ -1,9 +1,15 @@
 module Data.Algorithm.Sat.Fml (
-    toCNF,
-    vars,
-    prettyPrinter,
-    Fml (..),
-    mkVar
+    -- * Type
+    Fml (..)
+
+    -- * Creating
+    , mkVar
+    -- * Converting
+    , toCNF
+    -- * Querying
+    , vars
+    -- * Showing
+    , prettyPrinter
 ) where
 
 import qualified Data.Algorithm.Sat.Var as Var
@@ -20,38 +26,38 @@ data Fml a = Or     (Fml a) (Fml a)
 
 toCNF :: Fml a-> Fml a
 toCNF f
-    | Or a b <- f = Or (toCNF a) (toCNF b)
-    | And a b <- f = And (toCNF a) (toCNF b)
-    | Imply a b <- f = Or (Not (toCNF a)) (toCNF b)
-    | Equiv a b <- f = And (Or (toCNF a) (Not (toCNF b))) (Or (Not (toCNF a)) (toCNF b))
-    | XOr a b <- f = And (Or (toCNF a) (toCNF b)) (Or (Not (toCNF a)) (Not (toCNF b)))
-    | And a (Or b c) <- f = Or (And (toCNF a) (toCNF b)) (And (toCNF a) (toCNF c))
-    | Or a (And b c) <- f = And (Or (toCNF a) (toCNF b)) (Or (toCNF a) (toCNF c))
-    | Not a <- f = Not (toCNF a)
-    | Final a <- f = f
+    | Equiv a b         <- f = And (Or (toCNF a) (Not (toCNF b))) (Or (Not (toCNF a)) (toCNF b))
+    | XOr   a b         <- f = And (Or (toCNF a) (toCNF b)) (Or (Not (toCNF a)) (Not (toCNF b)))
+    | And   a (Or b  c) <- f = Or  (And (toCNF a) (toCNF b)) (And (toCNF a) (toCNF c))
+    | Or    a (And b c) <- f = And (Or  (toCNF a) (toCNF b)) (Or  (toCNF a) (toCNF c))
+    | Imply a b         <- f = Or  (Not (toCNF a)) (toCNF b)
+    | Or    a b         <- f = Or  (toCNF a) (toCNF b)
+    | And   a b         <- f = And (toCNF a) (toCNF b)
+    | Not   a           <- f = Not (toCNF a)
+    | Final a           <- f = f
 
 getVars :: Fml a -> [Var.Var a]
 getVars f
-    | Or a b <- f = getVars a ++ getVars b
-    | And a b <- f = getVars a ++ getVars b
+    | Or a b    <- f = getVars a ++ getVars b
+    | And a b   <- f = getVars a ++ getVars b
     | Imply a b <- f = getVars a ++ getVars b
     | Equiv a b <- f = getVars a ++ getVars b
-    | XOr a b <- f = getVars a ++ getVars b
-    | Not a <- f = getVars a
-    | Final a <- f = [a]
+    | XOr a b   <- f = getVars a ++ getVars b
+    | Not a     <- f = getVars a
+    | Final a   <- f = [a]
 
 vars :: (Eq a) => Fml a -> [Var.Var a]
 vars f = List.nub (getVars f)
 
 prettyPrinter :: (Show a) => Fml a -> String
 prettyPrinter f
-    | Or a b <- f = "(" ++ prettyPrinter a ++ " OR " ++ prettyPrinter b ++ ")"
-    | And a b <- f = "(" ++ prettyPrinter a ++ " AND " ++ prettyPrinter b ++ ")"
-    | Imply a b <- f = "(" ++ prettyPrinter a ++ " IMPLY " ++ prettyPrinter b ++ ")"
-    | Equiv a b <- f = "(" ++ prettyPrinter a ++ " EQUIV " ++ prettyPrinter b ++ ")"
-    | XOr a b <- f = "(" ++ prettyPrinter a ++ " XOR " ++ prettyPrinter b ++ ")"
-    | Not a <- f = "NOT " ++ prettyPrinter a
-    | Final a <- f = show a
+    | Or    a b <- f = "("    ++ prettyPrinter a ++ " OR "    ++ prettyPrinter b ++ ")"
+    | And   a b <- f = "("    ++ prettyPrinter a ++ " AND "   ++ prettyPrinter b ++ ")"
+    | Imply a b <- f = "("    ++ prettyPrinter a ++ " IMPLY " ++ prettyPrinter b ++ ")"
+    | Equiv a b <- f = "("    ++ prettyPrinter a ++ " EQUIV " ++ prettyPrinter b ++ ")"
+    | XOr   a b <- f = "("    ++ prettyPrinter a ++ " XOR "   ++ prettyPrinter b ++ ")"
+    | Not   a   <- f = "NOT " ++ prettyPrinter a
+    | Final a   <- f = show a
 
 mkVar :: a -> Fml a
 mkVar a = Final (Var.mk a)
