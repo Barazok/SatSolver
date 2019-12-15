08b082a23b7bfb6d594226c753383f28db388734
Ash Frost <barazok.blackrocks@gmail.com>
2019-12-15 16:54:59 +0100

    Starty working on Query

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,10 @@ module Main where
 import Data.Algorithm.Sat.Var
 import Data.Algorithm.Sat.Fml
 import Data.Algorithm.Sat.Fml.Examples
+import Data.Algorithm.Sat.Solver.CNFFml
+import Data.Algorithm.Sat.Solver.CNFFml.Clause
+import Data.Algorithm.Sat.Assignment
+import Data.Algorithm.Sat.Lit
 
 main :: IO ()
 main = putStrLn "hello world"
---
+++ src/Data/Algorithm/Sat/Query.hs
@@ -0,0 +1,18 @@
+module Data.Algorithm.Sat.Query (
+    -- satisfiable,
+    -- satisfyingAssignment,
+    -- satisfyingAssignments,
+    -- tautology
+    ) where
+
+-- satisfiable :: (Ord a) => Fml.Fml a -> Bool
+-- satisfiable
+
+-- satisfyingAssignment :: (Ord a) => Fml.Fml a -> Maybe (Assignment.Assignment a)
+-- satisfyingAssignment
+
+-- satisfyingAssignments :: (Ord a) => Fml.Fml a -> [Assignment.Assignment a]
+-- satisfyingAssignments
+
+-- tautology :: (Ord a) => Fml.Fml a -> Bool
+-- tautology
