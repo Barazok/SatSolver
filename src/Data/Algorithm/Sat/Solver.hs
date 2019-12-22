8da752399ed860b075ac28d9814a6a6380de228b
flopal <floret.palanque@hotmail.com>
2019-12-22 16:47:37 +0100

    Add first step to fromFml (not completed)

--- src/Data/Algorithm/Sat/Solver.hs
+++ src/Data/Algorithm/Sat/Solver.hs
@@ -8,6 +8,7 @@ import qualified Data.Algorithm.Sat.Fml as Fml
 import qualified Data.Algorithm.Sat.Assignment as Assignment
 import qualified Data.Algorithm.Sat.Lit as Lit
 import qualified Data.Algorithm.Sat.Solver.CNFFml as CNFFml
+import qualified Data.Algorithm.Sat.Solver.CNFFml.Clause as Clause
 import qualified Data.Map.Strict as Map
 
 
@@ -30,3 +31,12 @@ solve f = maximum (getVars (Fml.toCNF f))
 {- solve (Fml.Final a) = Just (Assignment.insert (Lit.mkTrue a) empty)
 solve (Fml.Not (Fml.Final a)) = Just (Assignment.insert (Lit.mkFalse a) empty)
 solve (Fml.Or a b) = solve a  -}
+
+fromFml :: (Eq a, Ord a) => Fml.Fml a -> CNFFml.CNFFml a
+fromFml f = CNFFml.mkCNFFml (aux (Fml.toCNF f))
+    where
+        aux (Fml.And a b) = aux a ++ aux b
+        aux (Fml.Or (Fml.Final a) (Fml.Final b)) = [Clause.mkClause [(aux1 a), (aux1 b)]]
+            where
+                aux1 c = Lit.mkTrue c
+        
