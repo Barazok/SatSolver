7996d4374532621bba5327e27870c409e62ab9c1
Ash Frost <barazok.blackrocks@gmail.com>
2019-12-22 21:10:15 +0100

    Implement satisfiable

--- src/Data/Algorithm/Sat/Query.hs
+++ src/Data/Algorithm/Sat/Query.hs
@@ -3,6 +3,8 @@ module Data.Algorithm.Sat.Query (
     satisfyingAssignment,
     satisfyingAssignments,
     truthtable,
+    getAllTrue,
+    extractAllFirst,
     -- tautology
     ) where
 
@@ -13,6 +15,8 @@ import qualified Data.Algorithm.Sat.Solver as Solver
 import qualified Data.Algorithm.Sat.Solver.CNFFml as CNFFml
 import qualified Data.Algorithm.Sat.Solver.CNFFml.Clause as Clause
 import qualified Data.Algorithm.Sat.Var as Var
+import qualified Data.Algorithm.Sat.Lit as Lit
+import qualified Data.List as List
 
 satisfiable :: (Ord a) => Fml.Fml a -> Bool
 satisfiable = Maybe.isJust . satisfyingAssignment
@@ -38,8 +42,17 @@ booltable (a:as) = [(a,b) : r | b <- bools, r <- booltable as]
 truthtable :: (Eq a) => Fml.Fml a -> [([(Var.Var a, Bool)], Bool)]
 truthtable e = [(bs, evaluate e bs) | bs <- booltable (Solver.getVars e)]
 
-satisfyingAssignments :: Ord a => Fml.Fml a -> [Assignment.Assignment a]
-satisfyingAssignments f = []
+getAllTrue :: (Eq a) => Fml.Fml a -> [([(Var.Var a, Bool)], Bool)]
+getAllTrue = filter (\(_,a) -> a == True) . truthtable . Fml.toCNF
+
+extractAllFirst :: (Eq a) => Fml.Fml a -> [(Var.Var a, Bool)]
+extractAllFirst f = let list = getAllTrue f in if length list == 0 then [] else [fst n | n <- list] !! 0
+
+satisfyingAssignments :: (Ord a) => Fml.Fml a -> [Assignment.Assignment a]
+satisfyingAssignments f = [aux n | n <- extractAllFirst f]
+    where
+        aux (v,True) = let assign = Assignment.mkEmpty in Assignment.insert (Lit.mkTrue v) assign
+        aux (v,False) = let assign = Assignment.mkEmpty in Assignment.insert (Lit.mkFalse v) assign
 
 -- tautology :: (Ord a) => Fml.Fml a -> Bool
 -- tautology
