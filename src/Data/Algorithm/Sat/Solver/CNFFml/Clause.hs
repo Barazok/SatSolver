1be54ef92b97346973d5cabc27df99d78c68b8ee
Ash Frost <barazok.blackrocks@gmail.com>
2019-12-22 17:51:18 +0100

    Modify print for clause and CNFFml

--- src/Data/Algorithm/Sat/Solver/CNFFml/Clause.hs
+++ src/Data/Algorithm/Sat/Solver/CNFFml/Clause.hs
@@ -11,7 +11,7 @@ import qualified Data.List as L
 -- A clause is a list of literals.
 newtype Clause a = Clause { getLits :: [Lit.Lit a] } deriving (Eq)
 instance (Show a) => Show (Clause a) where
-    show Clause { getLits = ls } = "(" ++ L.intercalate "," (L.map show ls) ++ ")"
+    show Clause { getLits = ls } = "(" ++ L.intercalate " | " (L.map show ls) ++ ")"
 
 mkClause :: (Ord a) => [Lit.Lit a] -> Clause a
 mkClause ls = Clause ls
