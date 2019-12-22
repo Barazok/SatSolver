7b3bc5f845dc02bcae8f565809ff1c31c99cc01c
Ash Frost <barazok.blackrocks@gmail.com>
2019-12-22 18:41:18 +0100

    Start working on query

--- src/Data/Algorithm/Sat/Query.hs
+++ src/Data/Algorithm/Sat/Query.hs
@@ -5,14 +5,21 @@ module Data.Algorithm.Sat.Query (
     -- tautology
     ) where
 
--- satisfiable :: (Ord a) => Fml.Fml a -> Bool
--- satisfiable
+import qualified Data.Maybe as Maybe
+import qualified Data.Algorithm.Sat.Assignment as Assignment
+import qualified Data.Algorithm.Sat.Fml as Fml
+import qualified Data.Algorithm.Sat.Solver as Solver
+import qualified Data.Algorithm.Sat.Solver.CNFFml as CNFFml
+import qualified Data.Algorithm.Sat.Solver.CNFFml.Clause as Clause
 
--- satisfyingAssignment :: (Ord a) => Fml.Fml a -> Maybe (Assignment.Assignment a)
--- satisfyingAssignment
+satisfiable :: (Ord a) => Fml.Fml a -> Bool
+satisfiable = Maybe.isJust . satisfyingAssignment
 
--- satisfyingAssignments :: (Ord a) => Fml.Fml a -> [Assignment.Assignment a]
--- satisfyingAssignments
+satisfyingAssignment :: (Ord a) => Fml.Fml a -> Maybe (Assignment.Assignment a)
+satisfyingAssignment = Maybe.listToMaybe . satisfyingAssignments
+
+satisfyingAssignments :: (Ord a) => Fml.Fml a -> [Assignment.Assignment a]
+satisfyingAssignments f = []
 
 -- tautology :: (Ord a) => Fml.Fml a -> Bool
 -- tautology
